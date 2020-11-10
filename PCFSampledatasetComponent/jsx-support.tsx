05126da26e3b98520797da209d8cdad66806d495
bartolomeo sorrentino <bartolomeo.sorrentino@gmail.com>
2020-11-10 23:36:03 +0100

    update get col value

--- PCFSampledatasetComponent/jsx-support.tsx
+++ PCFSampledatasetComponent/jsx-support.tsx
@@ -99,8 +99,8 @@ function createElement(tag:Tag, props: Props, ...children: Children):HTMLElement
                   {
                     ds.columns
                       //.filter( c => c.name !== undefined )
-                      //.map( c => <td>{ r.getValue(c.name) }</td> )
-                      .map( c => <td>{ `[${c.name}]` }</td> )
+                      .map( c => <td>{ `${r.getValue(c.displayName)}` }</td> )
+                      //.map( c => <td>{ `[${c.name}]` }</td> )
                     }
                 </tr>
               )
